d7ae23fa7e18c2270e8284198d0dd1def766599c
SanpetchBoriboon <sunpetch1997@gmail.com>
2025-03-01 14:26:22 +0700

    -

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -13,8 +13,7 @@ class OctopusBot {
     }
 
     async getUserProfile(userId) {
-        const userProfile =
-            await this.userProfileController.getUserProfile(userId);
+        const userProfile = await this.userProfileController.getUserProfile(userId);
         const lang = userProfile?.settingLanguage === 'en' ? 'en' : 'th';
         return { userProfile, lang };
     }
@@ -48,18 +47,8 @@ class OctopusBot {
             await ctx.reply(`${EN.CHOICE_LANGUAGE} | ${TH.CHOICE_LANGUAGE}`, {
                 reply_markup: {
                     inline_keyboard: [
-                        [
-                            {
-                                text: EN.LANGUAGE_OPTIONS['en'],
-                                callback_data: 'selected_en',
-                            },
-                        ],
-                        [
-                            {
-                                text: TH.LANGUAGE_OPTIONS['th'],
-                                callback_data: 'selected_th',
-                            },
-                        ],
+                        [{ text: EN.LANGUAGE_OPTIONS['en'], callback_data: 'selected_en' }],
+                        [{ text: TH.LANGUAGE_OPTIONS['th'], callback_data: 'selected_th' }],
                     ],
                 },
             });
@@ -75,107 +64,67 @@ class OctopusBot {
         const userId = ctx.chat.id;
         const { lang } = await this.getUserProfile(userId);
         const messages = lang === 'en' ? EN : TH;
-        const { MY_LOCATION, SHARE_LOCATION } = messages;
         const keyboard = {
-            keyboard: [
-                [
-                    {
-                        text: MY_LOCATION,
-                        request_location: true,
-                    },
-                ],
-            ],
+            keyboard: [[{ text: messages.MY_LOCATION, request_location: true }]],
             resize_keyboard: true,
             one_time_keyboard: true,
             remove_keyboard: true,
         };
 
-        await ctx.reply(SHARE_LOCATION, { reply_markup: keyboard });
+        await ctx.reply(messages.SHARE_LOCATION, { reply_markup: keyboard });
     }
 
     async handleLanguage(ctx) {
-        try {
-            const userId = ctx.chat.id;
-            const { userProfile, lang } = await this.getUserProfile(userId);
-            if (!userProfile) {
-                await ctx.reply('Please start the bot first');
-            } else {
-                const messages = lang === 'en' ? EN : TH;
-                const { LANGUAGE_OPTIONS, SETTING_LANGUAGE, CHOICE_LANGUAGE } =
-                    messages;
-                await ctx.reply(SETTING_LANGUAGE);
-                await ctx.reply(CHOICE_LANGUAGE, {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [
-                                {
-                                    text: LANGUAGE_OPTIONS['en'],
-                                    callback_data: 'en',
-                                },
-                            ],
-                            [
-                                {
-                                    text: LANGUAGE_OPTIONS['th'],
-                                    callback_data: 'th',
-                                },
-                            ],
-                        ],
-                    },
-                });
-            }
-        } catch (error) {
-            throw new Error(error);
+        const userId = ctx.chat.id;
+        const { userProfile, lang } = await this.getUserProfile(userId);
+        if (!userProfile) {
+            await ctx.reply('Please start the bot first');
+        } else {
+            const messages = lang === 'en' ? EN : TH;
+            await ctx.reply(messages.SETTING_LANGUAGE);
+            await ctx.reply(messages.CHOICE_LANGUAGE, {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{ text: messages.LANGUAGE_OPTIONS['en'], callback_data: 'en' }],
+                        [{ text: messages.LANGUAGE_OPTIONS['th'], callback_data: 'th' }],
+                    ],
+                },
+            });
         }
     }
 
     async handleIQAirLocation(ctx) {
-        try {
-            const userId = ctx.chat.id;
-            const { lang } = await this.getUserProfile(userId);
-            const { latitude, longitude } = ctx.message.location;
-            const { messageText } =
-                await this.iqairController.callAirQualityByLatLong(
-                    latitude,
-                    longitude,
-                    lang
-                );
-            await ctx.reply(messageText, { parse_mode: 'HTML' });
-        } catch (error) {
-            throw new Error(error);
-        }
+        const userId = ctx.chat.id;
+        const { lang } = await this.getUserProfile(userId);
+        const { latitude, longitude } = ctx.message.location;
+        const { messageText } = await this.iqairController.callAirQualityByLatLong(latitude, longitude, lang);
+        await ctx.reply(messageText, { parse_mode: 'HTML' });
     }
 
     async handleCallbackQuery(ctx) {
-        try {
-            const data = ctx.callbackQuery.data;
-            const userId = ctx.chat.id;
-            const langOptions = {
-                en: EN,
-                th: TH,
-            };
+        const data = ctx.callbackQuery.data;
+        const userId = ctx.chat.id;
+        const langOptions = { en: EN, th: TH };
 
-            if (data in langOptions) {
-                await this.userProfileController.settingLanguage(userId, data);
-                const selectedMessage = `${langOptions[data].LANGUAGE_SELECTED} ${langOptions[data].LANGUAGE_OPTIONS[data]}`;
-                await ctx.answerCallbackQuery({ text: selectedMessage });
-                await ctx.reply(selectedMessage);
-                await ctx.reply(langOptions[data].QUESTION_FOR_HELP);
-                await ctx.reply(langOptions[data].START);
-                await ctx.reply(langOptions[data].SETTING_LANGUAGE);
-            } else if (data.startsWith('selected_')) {
-                const selectedLang = data.split('_')[1];
-                await this.userProfileController.newUserProfile(userId, selectedLang)
-                    .then(async () => {
-                        await ctx.reply(langOptions[selectedLang].WELCOME);
-                        await ctx.reply(langOptions[selectedLang].START);
-                        await ctx.reply(langOptions[selectedLang].SETTING_LANGUAGE);
-                    })
-                    .catch((error) => {
-                        throw new Error(error);
-                    });
-            }
-        } catch (error) {
-            throw new Error(error);
+        if (data in langOptions) {
+            await this.userProfileController.settingLanguage(userId, data);
+            const selectedMessage = `${langOptions[data].LANGUAGE_SELECTED} ${langOptions[data].LANGUAGE_OPTIONS[data]}`;
+            await ctx.answerCallbackQuery({ text: selectedMessage });
+            await ctx.reply(selectedMessage);
+            await ctx.reply(langOptions[data].QUESTION_FOR_HELP);
+            await ctx.reply(langOptions[data].START);
+            await ctx.reply(langOptions[data].SETTING_LANGUAGE);
+        } else if (data.startsWith('selected_')) {
+            const selectedLang = data.split('_')[1];
+            await this.userProfileController.newUserProfile(userId, selectedLang)
+                .then(async () => {
+                    await ctx.reply(langOptions[selectedLang].WELCOME);
+                    await ctx.reply(langOptions[selectedLang].START);
+                    await ctx.reply(langOptions[selectedLang].SETTING_LANGUAGE);
+                })
+                .catch((error) => {
+                    throw new Error(error);
+                });
         }
     }
 }
